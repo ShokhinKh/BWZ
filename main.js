5f34ec17402e09da4c9ef0d31d142f369dd7d4d5
ShokhinKh <107809872+ShokhinKh@users.noreply.github.com>
2024-05-26 03:34:07 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,73 @@
-/* js */      
+/* js */   
+
+
+// Пролистывание до формы
 // Выберем кнопку и форму
-const button = document.querySelector('.button');
-const form = document.querySelector('.select');
+const button = document.querySelector('#book_button');
+const form = document.querySelector('#select');
 // При клике на кнопку
 button.addEventListener('click', e => {
   // Прокрутим страницу к форме 
   form.scrollIntoView({ 
-    block: 'nearest', // к ближайшей границе экрана
+    block: 'center', // к ближайшей границе экрана
     behavior: 'smooth', // и плавно 
   });
 });
 
+// Пролистывание до зон
 
+const button1 = document.querySelector('#zone_btn');
+const zone = document.querySelector('.zones');
 
-const button1 = document.querySelector('.nav__link1');
-const form1 = document.querySelector('.zones');
 button1.addEventListener('click', e => { 
-  form1.scrollIntoView({ 
-    block: 'nearest', 
-    behavior: 'smooth',
-  });
-});
-
-const button2 = document.querySelector('.nav__link2');
-const form2 = document.querySelector('.team');
-button2.addEventListener('click', e => { 
-  form2.scrollIntoView({ 
-    block: 'nearest', 
-    behavior: 'smooth',
-  });
-});
-
-const button3 = document.querySelector('.nav__link3');
-const form3 = document.querySelector('.reviews');
-button3.addEventListener('click', e => { 
-  form3.scrollIntoView({ 
-    block: 'nearest', 
-    behavior: 'smooth',
-  });
-});
-
-
-
-const button4 = document.querySelector('.button-zone');
-const form4 = document.querySelector('.input');
-button4.addEventListener('click', e => {
-  form4.scrollIntoView({ 
-    block: 'nearest', 
+  zone.scrollIntoView({ 
+    block: 'center',
     behavior: 'smooth', 
   });
 });
+
+// Пролистывание до команды
+
+const button2 = document.querySelector('#team_btn');
+const team = document.querySelector('.team');
+
+button2.addEventListener('click', e => { 
+  team.scrollIntoView({ 
+    block: 'center',
+    behavior: 'smooth', 
+  });
+});
+
+
+// Пролистывание до отзывов
+
+const button3 = document.querySelector('#reviews_btn');
+const reviews = document.querySelector('.reviews');
+
+button3.addEventListener('click', e => { 
+  reviews.scrollIntoView({ 
+    block: 'center',
+    behavior: 'smooth', 
+  });
+});
+
+// Модальное окно 
+
+const modal = document.getElementById("modal");
+const btn = document.getElementById("btn");
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+btn.onclick = openModal;
+
+window.onclick = function (e) {
+  if(e.target === modal)  {
+    closeModal();
+  }
+}
